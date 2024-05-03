eecb0b47fc3e27d7c1245d5205777545b6fc42d3
지운만 <manjee.official@gmail.com>
2024-05-03 11:54:06 +0900

    buildSrc 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.manjee.mumu"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = Configuration.versionCode
+        versionName = Configuration.versionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
