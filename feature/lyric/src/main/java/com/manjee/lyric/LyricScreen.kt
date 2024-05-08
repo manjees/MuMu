efac5644a182aed5c7628c717351622d0983c0c8
lucky <manjee.official@gmail.com>
2024-05-09 00:28:37 +0900

    UI 작업중

--- feature/lyric/src/main/java/com/manjee/lyric/LyricScreen.kt
+++ feature/lyric/src/main/java/com/manjee/lyric/LyricScreen.kt
@@ -1,12 +1,21 @@
+@file:OptIn(ExperimentalLayoutApi::class)
+
 package com.manjee.lyric
 
 import android.util.Log
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
@@ -20,7 +29,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.manjee.designsystem.ui.Yellow20
 import com.manjee.model.Lyric
 import kotlinx.coroutines.launch
 
@@ -49,13 +60,18 @@ internal fun LyricScreen() {
 
     LaunchedEffect(quizList) {
         quizList = quizList.toMutableList()
-        Log.i("@@@@", "$quizList")
     }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(Yellow20)
     ) {
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .height(250.dp)
+        )
+
         LazyColumn {
             itemsIndexed(
                 key = { index, item -> item.index },
@@ -115,8 +131,15 @@ internal fun LyricScreen() {
                 }
             }
         }
-        LazyColumn {
-            itemsIndexed(quizList) { index, item ->
+
+        FlowRow(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f),
+            horizontalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            repeat(quizList.size)  { index ->
+                val item = quizList[index]
                 val scale = remember { Animatable(1f) }
 
                 if (item.isVisible) {
