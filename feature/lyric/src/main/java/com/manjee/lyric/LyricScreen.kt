9c095dfc0676e06c13ca3b41f9f9fc5e9d45230b
Manjee <jeeunho@UNHOui-MacBookAir.local>
2024-05-09 16:23:07 +0900

    가사 퀴즈 화면 기본 디자인 완료

--- feature/lyric/src/main/java/com/manjee/lyric/LyricScreen.kt
+++ feature/lyric/src/main/java/com/manjee/lyric/LyricScreen.kt
@@ -2,38 +2,37 @@
 
 package com.manjee.lyric
 
-import android.util.Log
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.manjee.designsystem.ui.Yellow20
+import com.manjee.designsystem.ui.Green60
+import com.manjee.designsystem.ui.Grey90
+import com.manjee.designsystem.ui.Yellow10
+import com.manjee.lyric.component.LyricItem
 import com.manjee.model.Lyric
-import kotlinx.coroutines.launch
 
 @Composable
 fun LyricRoute() {
@@ -42,11 +41,12 @@ fun LyricRoute() {
 
 private var quizList by mutableStateOf(
     listOf(
-        Lyric(1, "1"),
-        Lyric(2, "2"),
-        Lyric(3, "3"),
-        Lyric(4, "4"),
-        Lyric(5, "5")
+        Lyric(1, "HI"),
+        Lyric(2, "HELLO"),
+        Lyric(3, "MY"),
+        Lyric(4, "NAME"),
+        Lyric(5, "IS"),
+        Lyric(6, "MANJEE")
     )
 )
 private var selectList by mutableStateOf(listOf<Lyric>())
@@ -55,136 +55,93 @@ private var selectList by mutableStateOf(listOf<Lyric>())
 internal fun LyricScreen() {
     val coroutineScope = rememberCoroutineScope()
 
-    var isAnswerListClick by remember { mutableStateOf(false) }
-    var isChoiceListClick by remember { mutableStateOf(false) }
-
-    LaunchedEffect(quizList) {
-        quizList = quizList.toMutableList()
-    }
-
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Yellow20)
+            .background(Yellow10)
     ) {
-        Box(modifier = Modifier
-            .fillMaxWidth()
-            .height(250.dp)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp)
+                .background(Green60)
         )
-
-        LazyColumn {
-            itemsIndexed(
-                key = { index, item -> item.index },
-                items = selectList
-            ) { _, item ->
-                val scale = remember { Animatable(1f) } // 초기화
-
-                Text(
-                    modifier = Modifier
-                        .scale(scale.value)
-                        .clickable {
-                            if (isAnswerListClick) {
-                                return@clickable
-                            }
-                            isAnswerListClick = true
-
-                            val job = coroutineScope.launch {
-                                scale.animateTo(
-                                    targetValue = if (scale.isRunning) 1f else 0f,
-                                    animationSpec = spring(
-                                        dampingRatio = Spring.DampingRatioMediumBouncy,
-                                        stiffness = Spring.StiffnessMedium
-                                    )
-                                )
-                            }
-
-                            job.invokeOnCompletion {
-                                quizList = quizList
-                                    .toMutableList()
-                                    .map {
-                                        if (it.index == item.index) {
-                                            it.copy(isVisible = true)
-                                        } else {
-                                            it
-                                        }
-                                    }
-
-                                selectList = selectList
-                                    .toMutableList()
-                                    .apply { remove(item) }
-
-                                isAnswerListClick = false
-//                                }
-                            }
-                        },
-                    text = item.str
-                )
-
-                LaunchedEffect(item) {
-                    scale.animateTo(
-                        targetValue = 1f,
-                        animationSpec = spring(
-                            dampingRatio = Spring.DampingRatioMediumBouncy,
-                            stiffness = Spring.StiffnessLow
-                        )
-                    )
-                }
-            }
-        }
-
+        Spacer(
+            modifier = Modifier
+                .height(4.dp)
+        )
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text = "Your Answer",
+            color = Grey90,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
         FlowRow(
             modifier = Modifier
                 .fillMaxSize()
-                .weight(1f),
-            horizontalArrangement = Arrangement.spacedBy(4.dp)
+                .weight(1f)
+                .padding(horizontal = 4.dp),
         ) {
-            repeat(quizList.size)  { index ->
-                val item = quizList[index]
-                val scale = remember { Animatable(1f) }
+            repeat(selectList.size) { index ->
+                val item = selectList[index]
 
-                if (item.isVisible) {
-                    Text(
-                        modifier = Modifier
-                            .scale(scale.value)
-                            .clickable {
-                                if (isChoiceListClick) {
-                                    return@clickable
+                LyricItem(
+                    lyric = item,
+                    onComplete = {
+                        quizList = quizList
+                            .toMutableList()
+                            .map {
+                                if (it.index == item.index) {
+                                    it.copy(isVisible = true)
+                                } else {
+                                    it
                                 }
-                                isChoiceListClick = true
+                            }
 
-                                val job = coroutineScope.launch {
-                                    scale.animateTo(
-                                        targetValue = 0f,
-                                        animationSpec = spring(
-                                            dampingRatio = Spring.DampingRatioMediumBouncy,
-                                            stiffness = Spring.StiffnessMedium
-                                        )
-                                    )
-                                }
+                        selectList = selectList
+                            .toMutableList()
+                            .apply { remove(item) }
+                    }
+                )
+            }
+        }
+        Card(
+            modifier = Modifier
+                .weight(1f)
+                .padding(16.dp),
+            colors = CardColors(
+                containerColor = Color.White,
+                contentColor = Color.White,
+                disabledContentColor = Color.White,
+                disabledContainerColor = Color.White
+            ),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 10.dp
+            )
+        ) {
+            FlowRow(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(15.dp),
+                horizontalArrangement = Arrangement.spacedBy(1.dp)
+            ) {
+                repeat(quizList.size) { index ->
+                    val item = quizList[index]
 
-                                job.invokeOnCompletion {
-                                    selectList = selectList
-                                        .toMutableList()
-                                        .apply { add(item) }
+                    if (item.isVisible) {
+                        LyricItem(
+                            coroutineScope = coroutineScope,
+                            lyric = item,
+                            onComplete = {
+                                selectList = selectList
+                                    .toMutableList()
+                                    .apply { add(item) }
 
-                                    quizList = quizList
-                                        .toMutableList()
-                                        .apply { set(index, item.copy(isVisible = false)) }
-
-                                    isChoiceListClick = false
-                                }
-                            },
-                        text = item.str,
-                        fontSize = 20.sp
-                    )
-
-                    LaunchedEffect(item) {
-                        scale.animateTo(
-                            targetValue = 1f,
-                            animationSpec = spring(
-                                dampingRatio = Spring.DampingRatioMediumBouncy,
-                                stiffness = Spring.StiffnessLow
-                            )
+                                quizList = quizList
+                                    .toMutableList()
+                                    .apply { set(index, item.copy(isVisible = false)) }
+                            }
                         )
                     }
                 }
---
+++ feature/lyric/src/main/java/com/manjee/lyric/component/LyricItem.kt
@@ -0,0 +1,91 @@
+package com.manjee.lyric.component
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.manjee.designsystem.ui.Grey90
+import com.manjee.model.Lyric
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Composable
+fun LyricItem(
+    coroutineScope: CoroutineScope = rememberCoroutineScope(), lyric: Lyric, onComplete: () -> Unit
+) {
+    val scale = remember { Animatable(1f) }
+    var isClicked: Boolean by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .padding(4.dp)
+    ) {
+        Button(
+            modifier = Modifier
+                .scale(scale.value)
+                .height(32.dp),
+            onClick = {
+                if (isClicked) return@Button
+                isClicked = true
+
+                val job = coroutineScope.launch {
+                    scale.animateTo(
+                        targetValue = 0f, animationSpec = spring(
+                            dampingRatio = Spring.DampingRatioMediumBouncy,
+                            stiffness = Spring.StiffnessMedium
+                        )
+                    )
+                }
+
+                job.invokeOnCompletion {
+                    onComplete()
+                    isClicked = false
+                }
+            }, colors = ButtonColors(
+                contentColor = Color.White,
+                containerColor = Grey90,
+                disabledContentColor = Color.White,
+                disabledContainerColor = Grey90
+            ), shape = RoundedCornerShape(8.dp), elevation = ButtonDefaults.buttonElevation(
+                defaultElevation = 10.dp
+            )
+        ) {
+            Text(
+                modifier = Modifier, text = lyric.str, color = Color.White
+            )
+        }
+    }
+
+    LaunchedEffect(key1 = lyric) {
+        scale.snapTo(
+            targetValue = 1f
+        )
+    }
+}
+
+@Composable
+@Preview
+fun LyricItemPreview() {
+    LyricItem(
+        lyric = Lyric(1, "Hello, world!")
+    ) {}
+}
